86362ced294b50e0f1a055129e12321b8e813b23
fixuberi <comeclarity007@gmail.com>
2018-09-11 16:43:18 +0300

    Add specs for NavigationHelper#collapsible_links_partial_path

---
+++ spec/helpers/navigation_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec. describe NavigationHelper, :type => :helper do
+  context 'signed-in user' do
+    before(:each) { helper.stub(:user_signed_in?).and_return(true) }
+
+    context '#collapsible_links_partial_path' do
+      it 'return signed-in user partial path' do
+        expect(helper.collapsible_links_partial_path).to eq 'layouts/navigation/collapsible_elements/signed_in_links'
+      end
+    end
+  end
+  context 'non-signed-in user' do
+    before(:each) { helper.stub(:user_signed_in?).and_return(false) }
+    context '#collapsible_links_partial_path' do
+      it 'return non-signed-in user partial path' do
+        expect(helper.collapsible_links_partial_path).to eq 'layouts/navigation/collapsible_elements/non_signed_in_links'
+      end
+    end
+  end
+end
