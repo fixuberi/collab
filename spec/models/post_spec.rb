f90914f6bf43e59314e76048eb16e7bd95addc79
fixuberi <comeclarity007@gmail.com>
2018-09-25 17:15:16 +0300

    add validation specs for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -41,12 +41,60 @@ RSpec.describe Post, :type => :model do
     end
 
     it 'search finds a matching post' do
-      post = create(:post, title: 'awesome title', content: 'great content ' * 5)
-      create_list(:post, 10, title: ('a'..'c' * 2).to_a.shuffle.join)
+      post = create(:post, title: 'super awesome title' * 2 , content: 'great content ' * 5)
+      create_list(:post, 10, title: ('a'..'z').to_a.shuffle.join)
       expect(Post.search('awesome').count).to eq 1
       expect(Post.search('awesome')[0].id).to eq post.id
       expect(Post.search('great').count).to eq 1
       expect(Post.search('great')[0].id).to eq post.id
+    end
+  end
+
+  context 'Validations' do
+    let(:post) { build(:post) }
+
+    it 'creates succesfully' do
+      expect(post).to be_valid
+    end
+
+    it 'is not valid without a category' do
+      post.category_id = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid without a title' do
+      post.title = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid  without a user_id' do
+      post.user_id = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid  with a title, shorter than 5 characters' do
+      post.title = 'a' * 4
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid  with a title, longer than 255 characters' do
+      post.title = 'a' * 260
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid without a content' do
+      post.content = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid  with a content, shorter than 20 characters' do
+      post.content = 'a' * 10
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid  with a content, longer than 1000 characters' do
+      post.content = 'a' * 1050
+      expect(post).not_to be_valid
     end
   end
 end
